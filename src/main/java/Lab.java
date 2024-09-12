ba80aa6d7c7e271cf8cc430d9c1e3de76cc25372
Daniel Olen <danolen89@gmail.com>
2024-09-12 15:21:56 +0000

    Coding Challenge: Return Array Contents Completed

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -31,7 +31,16 @@ public class Lab {
      * @param nums the array to be manipulated.
      * @return a string representation of all values of nums, with the values separated by spaces.
      */
-    public String arrayToString(int[] nums){
-        return "";
+    public String arrayToString(int[] nums) {
+        String result = ""; 
+
+        for (int i = 0; i < nums.length; i++) {
+            result += nums[i]; 
+
+            if (i < nums.length - 1) {
+                result += " "; 
+        }
+        
+        return result; 
     }
 }
